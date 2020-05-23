bfc873831b4dc2498aadaf1996b0b2777d8fa872
Russell Davis <551404+russelldavis@users.noreply.github.com>
2020-05-23 12:06:35 -0700

    ds_store logic
    abandoned, because Finder keeps .ds_store changes in memory for a while
    before flushing them

---
+++ trash101/__init__.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+ORIG_PATH_XATTR = "trash101_orig_path"
+TRASH_DIR = Path.home() / ".Trash"
--- trash101/putback.py
+++ trash101/putback.py
@@ -3,8 +3,42 @@ import os
 import sys
 from pathlib import Path
 from xattr import getxattr, removexattr
+from . import ORIG_PATH_XATTR, TRASH_DIR
 
-ORIG_PATH_XATTR = "trash101_orig_path"
+ds_file = None
+
+def get_ds_store_orig_path(trash_path):
+    if trash_path.parent != TRASH_DIR:
+        return None
+
+    global ds_file
+    if ds_file is None:
+        from ds_store import DSStore
+        # Closed implicitly when the process exits
+        ds_file = DSStore.open(str(TRASH_DIR / '.DS_Store'), 'r')
+
+    ds_entry = ds_file[trash_path.name]
+    print([x for x in ds_file])
+    try:
+        return ds_entry[b'ptbL'] + ds_entry[b'ptbN']
+    except KeyError:
+        return None
+
+def get_orig_path(trash_path):
+    try:
+        orig_path = getxattr(trash_path, ORIG_PATH_XATTR, symlink=True)
+    except IOError:
+        # getxattr doesn't distinguish failure modes
+        if not trash_path.exists():
+            sys.stderr.write(f"{trash_path}: No such file or directory\n")
+            return None
+
+        orig_path = get_ds_store_orig_path(trash_path)
+        if orig_path is None:
+            sys.stderr.write(
+                f"{trash_path}: could not restore: original path not available\n")
+        return orig_path
+    return os.fsdecode(orig_path)
 
 def main():
     if len(sys.argv) <= 1:
@@ -16,19 +50,11 @@ def main():
     exitcode = 0
     for arg in sys.argv[1:]:
         trash_path = Path(arg)
-
-        try:
-            orig_path = getxattr(trash_path, ORIG_PATH_XATTR, symlink=True)
-        except IOError:
-            # getxattr doesn't distinguish failure modes
-            if trash_path.exists():
-                sys.stderr.write(f"{trash_path}: could not restore: original path not available\n")
-            else:
-                sys.stderr.write(f"{trash_path}: No such file or directory\n")
+        orig_path = get_orig_path(trash_path)
+        if orig_path is None:
             exitcode = 1
             continue
 
-        orig_path = os.fsdecode(orig_path)
         try:
             orig_file = open(orig_path, "x")
         except FileExistsError:
