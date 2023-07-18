6c3206c1c52ac5c441f0861ad288f217666dab28
Russell Davis <551404+russelldavis@users.noreply.github.com>
2023-07-17 23:13:22 -0700

    Better handling of duplicate names and trash file cleanup after errors
    This handles the case of running rm **/node_modules, where there were
    two issues:
    
    1) Lots of folders with the same name being deleted during the same
    second. The old logic kept appending the datetime, resulting in
    "File name too long" errors.
    
    2) Lots of orphan stub folders in the trash due to lots of inner
    node_modules folders that end up in FileNotFoundError because the outer
    node_modules folder got trashed first. These weren't being cleaned up
    due to overly cautious logic when the source path doesn't exist. The
    better cautious logic just makes sure the trash path is of size 0
    (and the rmdir case already fails if the dir is not empty)

--- trash101/trash.py
+++ trash101/trash.py
@@ -14,6 +14,7 @@ def main():
         )
         sys.exit(64) # what rm does with no args
 
+    path_counts = {}
     exitcode = 0
     for arg in sys.argv[1:]:
         path = Path(arg)
@@ -28,14 +29,23 @@ def main():
             continue
 
         def on_error():
-            # It should still exist if there was an error, but to be safe,
-            # don't remove what's in the trash if the source isn't still there.
-            if path.exists():
-                if trashed_file:
-                    trashed_file.close()
-                    trashed_path.unlink()
-                else:
-                    trashed_path.rmdir()
+            if trashed_file:
+                trashed_file.close()
+                # This should never happen (the move operation should be atomic), but we want to
+                # make sure we don't delete data from the trash if there was an error *after* the
+                # file got moved.
+                if trashed_path.stat(follow_symlinks=False).st_size != 0:
+                    raise RuntimeError("File size in trash should be 0 after error")
+                trashed_path.unlink()
+                removed = True
+            else:
+                trashed_path.rmdir()
+                if trashed_path in path_counts:
+                    path_counts[trashed_path] -= 1
+
+            if trashed_path in path_counts:
+                path_counts[trashed_path] -= 1
+
             exitcode = 1
 
         while True:
@@ -45,8 +55,20 @@ def main():
                 else:
                     trashed_file = trashed_path.open("x")
             except FileExistsError:
-                # almost same format as macOS uses, except this has zero-padding on the hour
+                # Almost same format as macOS uses, except this has zero-padding on the hour.
                 trashed_path = trashed_path.with_name(trashed_path.name + strftime(" %I.%M.%S %p"))
+                # We also add this extra check if it already ends w/ this suffix -- otherwise,
+                # if you delete a bunch of files with the same name during the same second, you'll
+                # get the same timestamp appended a bunch of times, eventually resulting in a
+                # "File name too long" error (in Finder that's hard to do, which I guess is why
+                # Finder doesn't do this, but in the CLI it's easy, so we do).
+                #
+                # Only checking for this in memory, because it's easier and more efficient, and
+                # it's less likely to happen across multiple process invocations.
+                path_count = path_counts.get(trashed_path, 0)
+                path_counts[trashed_path] = path_count + 1
+                if path_count > 0:
+                    trashed_path = trashed_path.with_name(trashed_path.name + f" ({path_count})")
             else:
                 break
 
